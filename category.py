4e9038daf76c9e0a0f670edd14599f738ca0462e
Abasiono Alexander <ikangenyinabasiono@gmail.com>
2025-10-25 23:38:02 +0100

    Update category.py

--- category.py
+++ category.py
@@ -31,3 +31,20 @@ adult2 = pd.read_csv(
  dtype=adult_dtypes
 )
 print(adult2.dtypes)
+
+
+## updating data and converting data to categorical data
+# Create the update_coats dictionary
+update_coats = {
+    'wirehaired':'medium',
+    'medium-long': 'medium'
+}
+
+# Create a new column, coat_collapsed
+dogs["coat_collapsed"] = dogs['coat'].replace(update_coats)
+
+# Convert the column to categorical
+dogs['coat_collapsed'] = dogs['coat_collapsed'].astype('category')
+
+# Print the frequency table
+print(dogs['coat_collapsed'].value_counts())
